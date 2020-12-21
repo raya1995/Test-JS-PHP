25bb77a2505d84102e160d4c06efa5b0b164beab
raya1995 <raya.hadhri@esprit.tn>
2020-12-21 17:08:19 +0100

    Modifier

--- Ex2JS/ex2.js
+++ Ex2JS/ex2.js
@@ -15,44 +15,48 @@ var div= document.createElement("div");
 
 
 
+
   
-  
+ 
   
 var footer=document.createElement("footer");
 
-var id;
+  var id;
 
-footer.setAttribute(id,"monFooter");
-
-
-footer.classList="petit jaune";
+  footer.setAttribute(id,"monFooter");
 
 
-footer.style.width="300px";
-footer.style.height="200px";
-footer.style.position="relative";
-footer.style.backgroundColor="black";
+  footer.classList="petit jaune";
 
-var divGauche =footer.appendChild(document.createElement('div'));
+  
+  footer.style.width="300px";
+  footer.style.height="200px";
+  footer.style.position="relative";
+  footer.style.backgroundColor="black";
 
-divGauche.setAttribute(id,"divGauche");
-divGauche.classList="petit jaune";
+  var divGauche =footer.appendChild(document.createElement('div'));
 
-divGauche.style.width="100";
-divGauche.style.height="150";
-divGauche.style.position="absolute";
-divGauche.style.backgroundColor="red";
-divGauche.style.left=0;
-divGauche.style.top=0;
+  divGauche.setAttribute(id,"divGauche");
+  divGauche.classList="petit jaune";
 
-var divDroite =divGauche.appendChild(document.createElement('div'));
+  divGauche.style.width="100";
+  divGauche.style.height="150";
+  divGauche.style.position="absolute";
+  divGauche.style.backgroundColor="red";
+  divGauche.style.left=0;
+  divGauche.style.top=0;
+ 
+  var divDroite =footer.appendChild(document.createElement('div'));
 
-divDroite.setAttribute(id,"divDroite");
-divDroite.classList="petit jaune";
+  divDroite.setAttribute(id,"divDroite");
+  divDroite.classList="petit jaune";
 
-divDroite.style.width="100";
-divDroite.style.height="150";
-divDroite.style.position="absolute";
-divDroite.style.backgroundColor="blue";
-divDroite.style.left=0;
-divDroite.style.top=0;
+  divDroite.style.width="100";
+  divDroite.style.height="150";
+  divDroite.style.position="absolute";
+  divDroite.style.backgroundColor="blue";
+  divDroite.style.left=0;
+  divDroite.style.top=0;
+
+
+  console.log(footer)
