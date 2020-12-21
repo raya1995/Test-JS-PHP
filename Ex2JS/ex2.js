02637b9570b6ef1c06d05b8201d70eced26b1c1e
raya1995 <raya.hadhri@esprit.tn>
2020-12-21 17:05:05 +0100

    Testing

--- Ex2JS/ex2.js
+++ Ex2JS/ex2.js
@@ -11,3 +11,48 @@ var div= document.createElement("div");
   div.style.position="relative";
 
   div.textContent="hello world";
+
+
+
+
+  
+  
+  
+var footer=document.createElement("footer");
+
+var id;
+
+footer.setAttribute(id,"monFooter");
+
+
+footer.classList="petit jaune";
+
+
+footer.style.width="300px";
+footer.style.height="200px";
+footer.style.position="relative";
+footer.style.backgroundColor="black";
+
+var divGauche =footer.appendChild(document.createElement('div'));
+
+divGauche.setAttribute(id,"divGauche");
+divGauche.classList="petit jaune";
+
+divGauche.style.width="100";
+divGauche.style.height="150";
+divGauche.style.position="absolute";
+divGauche.style.backgroundColor="red";
+divGauche.style.left=0;
+divGauche.style.top=0;
+
+var divDroite =divGauche.appendChild(document.createElement('div'));
+
+divDroite.setAttribute(id,"divDroite");
+divDroite.classList="petit jaune";
+
+divDroite.style.width="100";
+divDroite.style.height="150";
+divDroite.style.position="absolute";
+divDroite.style.backgroundColor="blue";
+divDroite.style.left=0;
+divDroite.style.top=0;
